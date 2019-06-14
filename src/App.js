be788f50921bcea286fe936a9e1246ab48948b56
sunjin7725 <sunjin7725@gmail.com>
2019-06-14 17:19:48 +0900

    #1 make apollo client and get query`s data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
+import {HashRouter as Router, Route} from "react-router-dom"
+import {ApolloProvider} from "react-apollo";
+import client from "./apolloClient";
+import Detail from "./Detail";
+import Home from "./Home";
 
 class App extends Component {
   render(){
     return (
-      <div className="App">
-      
-      </div>
+        <ApolloProvider client={client}>
+          <Router>
+            <React.Fragment>
+              <Route exact={true} path={"/"} component={Home}></Route>
+              <Route path={"/details/:movieId"} component={Detail}></Route>
+            </React.Fragment>
+          </Router>
+        </ApolloProvider>
       );
   }
 }
